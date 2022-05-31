475c6403ac972a8ba841a87f47be841784287fd8
drayl <sunrise.161@mail.ru>
2022-05-31 18:48:34 +0300

    add layout for second activity

--- app/src/main/java/com/kilafyan/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/kilafyan/shoppinglist/presentation/MainActivity.kt
@@ -77,5 +77,4 @@ class MainActivity : AppCompatActivity() {
             viewModel.changeEnableState(it)
         }
     }
-
 }
---
+++ app/src/main/java/com/kilafyan/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.kilafyan.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kilafyan.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
