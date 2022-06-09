3d2fdf94cf950d7d62d792012600ff87f9dfdc4e
drayl <sunrise.161@mail.ru>
2022-06-09 16:35:24 +0300

    database and coroutines added to the project

--- app/src/main/java/com/kilafyan/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/kilafyan/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,57 +1,45 @@
 package com.kilafyan.shoppinglist.data
 
+import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.kilafyan.shoppinglist.domain.ShopItem
 import com.kilafyan.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
 import kotlin.random.Random
 
-object ShopListRepositoryImpl: ShopListRepository {
+class ShopListRepositoryImpl(application: Application): ShopListRepository {
 
-    private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
+    private val shopListDao = AppDataBase.getInstance(application).shopListDao()
+    private val mapper = ShopListMapper()
 
-    private var autoIncrementId: Long = 0
-
-    // FOR TESTS
-    init {
-        repeat(30) {
-            val item = ShopItem("name ${it + 1}", (it + 1).toDouble(), Random.nextBoolean())
-            addShopItem(item)
-        }
+    override suspend fun addShopItem(shopItem: ShopItem) {
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
-    override fun addShopItem(shopItem: ShopItem) {
-        if (shopItem.id == ShopItem.UNDEFINED_ID) {
-            shopItem.id = autoIncrementId++
-        }
-        shopList.add(shopItem)
-        updateList()
+    override suspend fun deleteShopItem(shopItem: ShopItem) {
+        shopListDao.deleteShopItem(shopItem.id)
     }
 
-    override fun deleteShopItem(shopItem: ShopItem) {
-        shopList.remove(shopItem)
-        updateList()
+    override suspend fun editShopItem(shopItem: ShopItem) {
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
-    override fun editShopItem(shopItem: ShopItem) {
-        val oldElement = getShopItem(shopItem.id)
-        shopList.remove(oldElement)
-        addShopItem(shopItem)
+    override suspend fun getShopItem(shopItemId: Long): ShopItem {
+        val dbModel = shopListDao.getShopItem(shopItemId)
+        return mapper.mapDbModelToEntity(dbModel)
     }
 
-    override fun getShopItem(shopItemId: Long): ShopItem {
-        return  shopList.find { it.id == shopItemId } ?:
-            throw RuntimeException("Element with id $shopItemId not found")
-    }
-
-    override fun getShopList(): LiveData<List<ShopItem>> {
-        return shopListLD
+    override fun getShopList(): LiveData<List<ShopItem>> = Transformations.map(
+        shopListDao.getShopList()
+    ) {
+        mapper.mapListDbModelToListEntity(it)
     }
 
     private fun updateList() {
-        shopListLD.value = shopList.toList()
+//        shopListLD.value = shopList.toList()
     }
 
 }
--- app/src/main/java/com/kilafyan/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/kilafyan/shoppinglist/presentation/MainViewModel.kt
@@ -1,16 +1,23 @@
 package com.kilafyan.shoppinglist.presentation
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kilafyan.shoppinglist.data.ShopListRepositoryImpl
 import com.kilafyan.shoppinglist.domain.DeleteShopItemUseCase
 import com.kilafyan.shoppinglist.domain.EditShopItemUseCase
 import com.kilafyan.shoppinglist.domain.GetShopListUseCase
 import com.kilafyan.shoppinglist.domain.ShopItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel(application: Application): AndroidViewModel(application) {
 
-    private val repository = ShopListRepositoryImpl
+    private val repository = ShopListRepositoryImpl(application)
 
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
@@ -18,17 +25,16 @@ class MainViewModel: ViewModel() {
 
     val shopList = getShopListUseCase.getShopList()
 
-
-
     fun deleteShopItem(shopItem: ShopItem) {
-        deleteShopItemUseCase.deleteShopItem(shopItem)
+        viewModelScope.launch {
+            deleteShopItemUseCase.deleteShopItem(shopItem)
+        }
     }
 
     fun changeEnableState(shopItem: ShopItem) {
-        val item = shopItem.copy(enable = !shopItem.enable)
-        editShopItemUseCase.editShopItem(item)
+        viewModelScope.launch {
+            val item = shopItem.copy(enable = !shopItem.enable)
+            editShopItemUseCase.editShopItem(item)
+        }
     }
-
-
-
 }
